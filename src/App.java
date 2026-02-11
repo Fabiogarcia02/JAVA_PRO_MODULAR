65c311e0638a3a3c82a5176837d353fdd7e3db44
Fabio Garcia <fabiogarciamartins2019@gmail.com>
2026-02-11 10:19:02 -0300

    Media e soma

--- src/App.java
+++ src/App.java
@@ -1,5 +1,18 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    
+    public class App {
+        public static void main(String[] args) {
+            int n;
+            int n1;
+            int soma;
+            double media;
+            IO.println("Digite uma quantidade de números para mostrar a soma e a média:");
+
+            n= Integer.parseInt(IO.readln("Digite:"));
+            for(int i=0; i<n; i++){
+         IO.readln("Digite o numero:"+i+1);
+                soma+= n1;
+            }
+
+            IO.println("Você digitou: " + n);
+        }
     }
-}
